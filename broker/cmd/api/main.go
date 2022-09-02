189ea18a77d27acbd91bb1fd3742eadefd9068e3
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-09-02 01:03:24 -0300

    chore: set up cors

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = "3000"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	fmt.Println("Starting on port:", PORT)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
---
+++ broker/cmd/api/routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedHeaders:   []string{"Accept", "Content-Type", "Authorization", "X-CSRF-Token"},
+		AllowedMethods:   []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
