d391cb1a1cf433d01828f6c305278b20beed1aad
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-09-02 04:40:09 -0300

    feat: add errorJson mtd to broker helpers

--- broker/cmd/api/helpers.go
+++ broker/cmd/api/helpers.go
@@ -7,6 +7,12 @@ import (
 	"net/http"
 )
 
+type jsonResponse struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
 func (app *Config) readJson(w http.ResponseWriter, r *http.Request, data any) error {
 	maxByteSize := 1048576
 
@@ -47,4 +53,18 @@ func (app *Config) writeJson(w http.ResponseWriter, status int, data any, header
 	}
 
 	return nil
+}
+
+func (app *Config) errorJson(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJson(w, statusCode, payload)
 }
